16cb119dbad71725298798c2252352d1f8fba5d1
zidoopal <yuzzzin@gmail.com>
2023-10-18 19:40:40 +0900

    Feat: react-toastify 컨테이너 생성
    - 퀴즈생성 페이지에 용량제한 알림 toast 생성
    - 추후에 위치 옮겨야 할듯..싶어요..

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,26 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { RecoilRoot } from 'recoil';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <RecoilRoot>
     <App />
+    <ToastContainer
+      position="top-center"
+      autoClose={3000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="colored"
+    />
   </RecoilRoot>,
 );
