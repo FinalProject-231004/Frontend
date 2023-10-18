5203d1e4db17b72eaf413c5fb04763586bfb0a0f
zidoopal <yuzzzin@gmail.com>
2023-10-18 19:23:55 +0900

    Design: 전역 폰트 설정
    - index.css 설정
    - tailwind.config.ts 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,21 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        navy: '#0e2958',
+        blue: '#0078ff',
+        offwhite: '#f4f4f4',
+        deep_dark_gray: '#3e3e3e',
+      },
+    },
+    fontFamily: {
+      tmoney: ['TmoneyRoundWind', 'sans-serif'],
+    },
+    fontWeight: {
+      regular: 400,
+      extrabold: 800,
+    },
   },
   plugins: [],
 };
