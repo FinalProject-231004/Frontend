e29bf8ce9c3d487991d4b7752d78681349462ba9
zidoopal <yuzzzin@gmail.com>
2023-10-20 10:56:10 +0900

    Chore: Quiz 타입 일부 수정

--- src/types/home.ts
+++ src/types/home.ts
@@ -3,7 +3,7 @@ export type Banner = {
   category: string;
 };
 
-export type QuizData = {
+export type Quiz = {
   id: number;
   viewNum: number;
   likes: number;
@@ -14,15 +14,14 @@ export type QuizData = {
 };
 
 export type QuizThumbnailProps = {
-  quiz: QuizData;
+  quiz: Quiz;
 };
 
 export type QuizCategorySectionProps = {
   title: string;
-  quiz: QuizData[];
+  quiz: Quiz[] | null;
 };
 
 export type Category = {
-  id: number;
-  name: string;
+  category: string;
 };
