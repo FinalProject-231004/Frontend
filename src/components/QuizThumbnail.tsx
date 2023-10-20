2cdc6767e545b814cb81d652c45374a922e0c8fd
zidoopal <yuzzzin@gmail.com>
2023-10-20 11:02:05 +0900

    Feat: 좋아요 버튼 추가

--- src/components/QuizThumbnail.tsx
+++ src/components/QuizThumbnail.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { QuizThumbnailProps } from '@/types/home';
+import { BiLike, BiSolidLike } from 'react-icons/bi';
+import { FaRegEye } from 'react-icons/fa';
+import { useLike } from '@/hooks/useLike';
 
 const QuizThumbnail: React.FC<QuizThumbnailProps> = ({ quiz }) => {
+  const { isLiked, likes, handleLike } = useLike(quiz.id);
+
   return (
     <div className="w-[347px]">
-      <h4 className="text-lg mt-2">{quiz.category}</h4>
+      <h4 className="hidden">{quiz.category}</h4>
       <h4 className="text-lg mt-2">{quiz.title}</h4>
       <img
         src={quiz.image}
@@ -13,10 +18,16 @@ const QuizThumbnail: React.FC<QuizThumbnailProps> = ({ quiz }) => {
       />
       <div className="flex mt-2 justify-between">
         <div>{quiz.username}</div>
-        <div className="flex">
-          <div>💕{quiz.likes}</div>
-          <div>👁‍🗨{quiz.viewNum}</div>
-        </div>
+
+        <button onClick={handleLike}>
+          {isLiked ? <BiSolidLike size={25} /> : <BiLike size={25} />}
+          {likes || 0}
+        </button>
+
+        <button>
+          <FaRegEye size={25} />
+          {quiz.viewNum}
+        </button>
       </div>
     </div>
   );
