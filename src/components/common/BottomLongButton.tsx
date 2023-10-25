2935ae60b968e2df4b2d7f65e88f33bad7ddf813
zidoopal <yuzzzin@gmail.com>
2023-10-26 02:03:49 +0900

    Feat: 공통컴포넌트 / 하단 긴 버튼 컴포넌트 생성

---
+++ src/components/common/BottomLongButton.tsx
@@ -0,0 +1,22 @@
+type BottomLongButtonProps = {
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  children: React.ReactNode;
+};
+const BottomLongButton: React.FC<BottomLongButtonProps> = ({
+  onClick,
+  children,
+}) => {
+  return (
+    <div className="w-[1080px] fixed bottom-0  mx-auto bg-white">
+      <button
+        type="button"
+        className="w-full h-[80px] bg-blue font-extrabold text-[26px] text-white py-3"
+        onClick={onClick}
+      >
+        {children}
+      </button>
+    </div>
+  );
+};
+
+export default BottomLongButton;
