137768a90e35f9e50d8b00f3e3ec80f92f00fa3a
wynter24 <m.wynter.k@gmail.com>
2023-10-27 17:03:02 +0900

    Feat: 퀴즈만들기 이동 로직 작성중

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -3,10 +3,20 @@ import { isLoggedInState } from '@/recoil/atoms/loggedHeaderAtom';
 import { Link } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import { LoggedInHeader } from '@/components';
+import { useModalState } from '@/hooks';
+import Modal from 'react-modal';
 
 function Header() {
   const isLoggedIn = useRecoilValue(isLoggedInState);
-  // const token = localStorage.getItem('Authorization');
+  const confirmLoginModal = useModalState();
+
+  const handleQuizCreateClick = () => {
+    if (!isLoggedIn) {
+      confirmLoginModal.open();
+    } else {
+      // 로그인한 상태라면 퀴즈 만들기 페이지로 이동하거나 원하는 작업을 수행합니다.
+    }
+  };
 
   return (
     <>
@@ -14,10 +24,10 @@ function Header() {
       <div className="w-full justify-center h-[72px] fixed top-0 px-[420px] bg-white flex items-center shadow-sm ">
         <div className="flex justify-between w-[1080px]">
           <div className="flex items-center">
-            <Link to="" className="mr-[37px]">
+            <Link to="/" className="mr-[37px]">
               로고
             </Link>
-            <Link to="" className="mr-[37px]">
+            <Link onClick={handleQuizCreateClick} to="/create-quiz/details" className="mr-[37px]">
               퀴즈만들기
             </Link>
             <Link to="" className="mr-[37px]">
@@ -29,6 +39,18 @@ function Header() {
           </div>
           {isLoggedIn ? <LoggedInHeader /> : <SignInModal />}
         </div>
+
+        <Modal
+          isOpen={confirmLoginModal.isOpen}
+          onRequestClose={confirmLoginModal.close}
+          contentLabel="로그인이 필요한 서비스입니다."
+        >
+          <p>로그인이 필요한 서비스입니다.</p>
+          <button onClick={confirmLoginModal.close}>닫기</button>
+          {/* 모달 안에 로그인 컴포넌트 또는 로그인 모달을 추가할 수 있습니다. */}
+          {/* 예: <SignInModal /> */}
+        </Modal>
+
       </div>
     </>
   );
--- src/components/index.ts
+++ src/components/index.ts
@@ -38,9 +38,9 @@ export { default as HistoryList } from './MyPage/HistoryList';
 export { default as UpdateMyInfo } from './MyPage/UpdateMyInfo';
 
 // Root
-export { default as HomeBanner } from './HomeBanner';
-export { default as QuizCategorySection } from './QuizCategorySection';
-export { default as QuizThumbnail } from './QuizThumbnail';
-export { default as QuizThumbnailModal } from './QuizThumbnailModal';
+export { default as HomeBanner } from './Home/HomeBanner';
+export { default as QuizCategorySection } from './Home/QuizCategorySection';
+export { default as QuizThumbnail } from './Home/QuizThumbnail';
+export { default as QuizThumbnailModal } from './Home/QuizThumbnailModal';
 export { default as CurrentDate } from './CurrentDate';
 
