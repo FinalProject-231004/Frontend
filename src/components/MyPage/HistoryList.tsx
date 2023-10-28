507ef2cab3a2041c6892278df82e90296628f179
wynter24 <m.wynter.k@gmail.com>
2023-10-28 12:37:01 +0900

    Design: MyPage CSS 커스텀

--- src/components/MyPage/HistoryList.tsx
+++ src/components/MyPage/HistoryList.tsx
@@ -21,3 +21,42 @@ export default function HistoryList({price, cost, itemName, quantity, email, dat
     </div>
   )
 }
+
+// import { historyListProps } from '@/types/myPage';
+
+// export default function HistoryList({price, cost, itemName, quantity, email, date}: historyListProps) {
+  
+//   // 스타일 정의
+//   const baseStyle: React.CSSProperties = {
+//     minWidth: "13px",
+//     maxWidth: "278px",
+//   };
+//   const priceStyle = {
+//     ...baseStyle,
+//     color: price === 'reward' ? '#0078FF' : '#FF3D00',
+//     fontSize: '22px'
+//   };
+//   const textStyle = {
+//     ...baseStyle,
+//     color: '#0078FF',
+//     fontSize: '22px',
+//   };
+//   const dateStyle = {
+//     ...baseStyle,
+//     color: '#D3D3D3',
+//     fontSize: '22px'
+//   };
+
+//   return (
+//     <div className='w-[998px] h-[70px] py-[23px] pl-[25px] pr-[14px] border-b border-blue flex justify-between items-center'>
+//       <p style={priceStyle}>
+//         {price === 'reward' ? `+${cost}M` : `-${cost}원`}
+//       </p>
+      
+//       <p style={textStyle}>{itemName || '데이터 없음'}</p>
+//       <p style={textStyle}>{quantity || ''}</p>
+//       <p style={textStyle}>{email || ''}</p>
+//       <p style={dateStyle}>{date || ''}</p>
+//     </div>
+//   );
+// }
