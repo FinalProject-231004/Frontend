e8df94a96f4413b4d3cb8a13af82cb8f915ebb33
zidoopal <yuzzzin@gmail.com>
2023-10-31 20:01:03 +0900

    Design: 퀴즈 생성 1,2 페이지 경고 모달 CSS 수정

--- src/components/common/WarningModal.tsx
+++ src/components/common/WarningModal.tsx
@@ -18,24 +18,21 @@ const WarningModal: React.FC<WarningModalProps> = ({
   <Modal
     isOpen={isOpen}
     onRequestClose={onRequestClose}
+    shouldCloseOnOverlayClick={false}
     contentLabel="Warning"
-    style={{
-      overlay: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
-      content: {
-        width: '300px',
-        height: '150px',
-        textAlign: 'center',
-        margin: 'auto',
-      },
-    }}
+    className="w-[713px] h-[180px] inline-block align-bottom bg-[#F1F8FF] rounded-lg text-left overflow-hidden shadow-md shadow-slate-400 transform transition-all sm:my-8 sm:align-middle max-w-md p-6"
+    overlayClassName="fixed inset-0 bg-gray-300 bg-opacity-75 transition-opacity flex justify-center items-center"
   >
-    <h2>{title}</h2>
-    <p>{message}</p>
-    <div>{buttons}</div>
+    <div className="sm:flex flex-col justify-center items-center">
+      <h3 className="text-3xl">{title}</h3>
+    </div>
+    <p className="text-blue font-extrabold mt-3 text-center">{message}</p>
+    <button
+      className="text-sm mt-6 sm:flex bg-blue p-2 rounded-lg text-white mx-auto"
+      type="button"
+    >
+      {buttons}
+    </button>
   </Modal>
 );
 
