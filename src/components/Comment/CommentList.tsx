2f38245d20c0661de913068cc1da74138263f050
zidoopal <yuzzzin@gmail.com>
2023-11-01 04:15:09 +0900

    Design: 댓글 컴포넌트 CSS 수정

--- src/components/Comment/CommentList.tsx
+++ src/components/Comment/CommentList.tsx
@@ -10,10 +10,11 @@ const CommentList: React.FC<CommentProps> = ({
   commentData,
   isLastComment,
 }) => {
+  console.log(commentData);
   return (
-    <div className="flex mb-8 justify-center items-center">
-      <img src="/profile.png" className="w-[65px]" alt={`profile`} />
-      <div className="ml-6">
+    <div className="w-full flex mb-7 justify-center items-center">
+      <img src="/profile.png" className="w-[60px]" alt={`profile`} />
+      <div className="w-full ml-4">
         <div className="flex justify-between items-center">
           <div className="flex mb-1 text-sm px-2 justify-start items-center rounded-md bg-blue text-white">
             {commentData.nickname}
@@ -22,11 +23,9 @@ const CommentList: React.FC<CommentProps> = ({
             {commentData.createdAt.split('T')[0]}
           </div>
         </div>
-        <div className="w-[438px] h-[50px] flex customborder">
-          {commentData.comment}
-        </div>
+        <div className="h-[50px] flex customborder">{commentData.comment}</div>
         {isLastComment && (
-          <div className="text-xs text-center text-slate-400">
+          <div className="text-xs text-center text-slate-300">
             마지막 댓글입니다!
           </div>
         )}
