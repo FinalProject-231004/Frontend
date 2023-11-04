d4365c7371e0d5a45128eb16687a9ea00f1b800f
zidoopal <yuzzzin@gmail.com>
2023-11-04 11:10:53 +0900

    Chore: console.log 삭제

--- src/components/common/ShareModal.tsx
+++ src/components/common/ShareModal.tsx
@@ -21,7 +21,7 @@ const ShareModal: React.FC<ShareModalProps> = ({
 
   const shareKakaoLink = (pathType: string) => {
     const Url = pathType === 'detail' ? `quiz/${id}` : `quiz/${id}`;
-    console.log(Url);
+    // console.log(Url);
     window.Kakao.Share.sendCustom({
       templateId: 100262,
       templateArgs: {
