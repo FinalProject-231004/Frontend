dd7493a27181c3cfcc09d9bf2d07bb4cb33fdb77
wynter24 <m.wynter.k@gmail.com>
2023-11-08 04:32:27 +0900

    Design: 프로필 이미지 변경 기본 이미지와 헤더 기본 프로필 사진과 통일

--- src/components/MyPage/ChangeProfile.tsx
+++ src/components/MyPage/ChangeProfile.tsx
@@ -66,7 +66,7 @@ export default function ChangeProfile({profileImg}:changeProfileProps) {
   return (
     <div className="flex flex-col items-center justify-center">
         <div className="w-[222px] h-[222px] flex items-center justify-center rounded-full overflow-hidden">
-            <img src={imgFile || profileImg || 'https://ifh.cc/g/LLB0LN.png'} alt="profile" className="max-w-full h-auto" />
+            <img src={imgFile || profileImg || '/img/bonobono.png'} alt="profile" className="max-w-full h-auto" />
         </div>
  
         <div className="mt-[19px] flex justify-between items-center">
