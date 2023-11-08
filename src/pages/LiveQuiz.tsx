374a1b98d5960c4601bfd2254476e50320a815a6
zidoopal <yuzzzin@gmail.com>
2023-11-08 22:32:39 +0900

    Feat: 라이브 퀴즈 페이지 & 유저상태 Recoil Atom 생성

---
+++ src/pages/LiveQuiz.tsx
@@ -0,0 +1,11 @@
+import { LiveQuizComp } from '@/components';
+
+const LiveQuiz: React.FC = () => {
+  return (
+    <div className="w-screen h-[1080px]">
+      <LiveQuizComp />
+    </div>
+  );
+};
+
+export default LiveQuiz;
