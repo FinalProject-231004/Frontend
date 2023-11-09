e5dbe26bf0a079b2aa37721f9c0829150fad5d5c
wynter24 <m.wynter.k@gmail.com>
2023-11-09 16:57:36 +0900

    Feat: react-responsive 이용한 모바일 크기 설정 커스텀 훅 생성

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -9,6 +9,7 @@ export { useHorizontalScroll } from './useHorizontalScroll';
 export { useDebounce } from './useDebounce';
 export { usePlayQuiz } from './usePlayQuiz';
 export { useUserLoggedIn } from './useUserLoggedIn';
+export { useMobile } from './useMobile ';
 export {
   useGetMessageAlert,
   usePutReadAlert,
