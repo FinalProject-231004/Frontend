e941178a48ed51bb8b6ad29906a7ceb171087f03
wynter24 <m.wynter.k@gmail.com>
2023-11-13 02:19:13 +0900

    Design: 모바일 회원탈퇴 모달 완성

--- src/components/MyPage/DeleteAccount.tsx
+++ src/components/MyPage/DeleteAccount.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { CustomizedButtons, Modal, UserInfoInput } from '@/components';
-import { useModalState } from '@/hooks';
+import { useMobile, useModalState } from '@/hooks';
 import { deleteAPI } from '@/apis/axios';
 import { deletePw } from '@/types/myPage';
 import { useNavigate } from 'react-router';
@@ -15,6 +15,7 @@ export default function DeleteAccount() {
   const [enterPassword, setInputPw] = useState('');
   const [isPw, setIsPw] = useState(false);
   const [pwMessage, setPwMessage] = useState('');
+  const isMobile = useMobile(); 
 
   const pwData = {
     enterPassword: enterPassword,
@@ -59,41 +60,40 @@ export default function DeleteAccount() {
       </button>
       <Modal
         onRequestClose={closeModal}
-        width="713px"
-        height="368px"
+        width={!isMobile? '713px' : '356px'}
+        height={!isMobile? '368px' : '184px'}
         bgColor="#0E2958"
         isOpen={deleteAccountModal.isOpen}
       >
-        <div className="h-[368px] flex flex-col justify-center items-center">
-          <h1 className="mb-[46px] text-[34px] text-blue">
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="py-[46px] text-[34px] text-blue font-extrabold sm:text-[16px] sm:py-[23px]">
             정말 탈퇴하실 건가요?
           </h1>
+          <div className='relative'>
+            <div className="relative flex justify-center items-center z-10">
+              <UserInfoInput
+                inputVal={enterPassword}
+                type="password"
+                placeholder="비밀번호"
+                size="medium"
+                borderColor="blue"
+                focusBorderColor={''}
+                onChange={e => {
+                  setInputPw(e.target.value);
+                  validatePw(e.target.value);
+                }}
+              />
+              {isPw && (
+                <i className="far fa-circle-xmark absolute top-18 right-[17px] text-[37px] text-red z-20 sm:text-base"></i>
+              )}
+            </div>
 
-          <div className="relative flex justify-center items-center z-10">
-            <UserInfoInput
-              inputVal={enterPassword}
-              type="password"
-              placeholder="비밀번호"
-              size="medium"
-              borderColor="blue"
-              focusBorderColor={''}
-              onChange={e => {
-                setInputPw(e.target.value);
-                validatePw(e.target.value);
-              }}
-            />
-
-            {isPw && (
-              <i className="fa-regular fa-circle-xmark absolute top-18 right-[17px] h-[37px] w-[37px] text-[37px] text-[#F92316] z-20"></i>
-            )}
-          </div>
-          <div className="relative w-[530px]">
-            <div className="absolute right-0 my-[6px] text-[12px] text-white font-hairline">
+            <div className="my-[6px] absolute right-0 text-[16px] text-white font-hairline sm:text-[10px] sm:my-[3px]">
               {pwMessage}
             </div>
           </div>
 
-          <div className="mt-[60px]">
+          <div className="pt-[60px] sm:pt-[30px]">
             <CustomizedButtons
               size="large"
               fontcolor="white"
